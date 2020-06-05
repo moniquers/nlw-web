665bf7e1589eebee06d467d9a46bbbd8334a83bb
Monique Ribeiro de Sousa <ribeiromonique90@gmail.com>
2020-06-05 18:56:27 -0300

    define posição inicial pela localização atual do usuário e adiciona opção de selecionar localidade no mapa

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import './styles.css';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi';
+import { LeafletMouseEvent } from 'leaflet';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api'
 import axios from 'axios';
@@ -33,6 +34,15 @@ const CreatePoint = () => {
 
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const {latitude,longitude} = position.coords;
+            setInitialPosition([latitude,longitude]);
+        })
+    },[])
 
     useEffect(() => {
         api.get('items')
@@ -66,89 +76,95 @@ const CreatePoint = () => {
         const city = event.target.value;
         setSelectedCity(city);
     }
+    function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPosition([
+            event.latlng.lat,
+            event.latlng.lng
+        ])
+    }
 
-    return (
-        <div id="page-create-point">
-            <header>
-                <img src={logo} alt="Ecoleta" />
-                <Link to="/">
-                    <FiArrowLeft />
+return (
+    <div id="page-create-point">
+        <header>
+            <img src={logo} alt="Ecoleta" />
+            <Link to="/">
+                <FiArrowLeft />
                         Voltar para a home
                 </Link>
-            </header>
-            <form>
-                <h1>Cadastro do<br />ponto de coleta</h1>
-                <fieldset>
-                    <legend>
-                        <h2>Dados</h2>
-                    </legend>
+        </header>
+        <form>
+            <h1>Cadastro do<br />ponto de coleta</h1>
+            <fieldset>
+                <legend>
+                    <h2>Dados</h2>
+                </legend>
+                <div className="field">
+                    <label htmlFor="name">Nome da entidade</label>
+                    <input type="text" name="name" id="name" />
+                </div>
+                <div className="field-group">
                     <div className="field">
-                        <label htmlFor="name">Nome da entidade</label>
-                        <input type="text" name="name" id="name" />
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" name="email" id="email" />
                     </div>
-                    <div className="field-group">
-                        <div className="field">
-                            <label htmlFor="email">E-mail</label>
-                            <input type="email" name="email" id="email" />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="name">Whatsapp</label>
-                            <input type="text" name="whatsapp" id="whatsapp" />
-                        </div>
+                    <div className="field">
+                        <label htmlFor="name">Whatsapp</label>
+                        <input type="text" name="whatsapp" id="whatsapp" />
                     </div>
-                </fieldset>
+                </div>
+            </fieldset>
 
-                <fieldset>
-                    <legend>
-                        <h2>Endereço</h2>
-                        <span>Selecione o endereço no mapa</span>
-                    </legend>
-                    <Map center={[-23.6017817, -46.6922771]} zoom={15} >
-                        <TileLayer
-                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        ></TileLayer>
-                        <Marker position={[-23.6017817, -46.6922771]}></Marker>
-                    </Map>
-                    <div className="field-group">
-                        <div className="field">
-                            <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf" value={selectedUf} onChange={handleSelectUf}>
-                                <option value="0">Selecione uma UF</option>
-                                {ufs.map(uf => (
-                                    <option key={uf} value={uf}>{uf}</option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className="field">
-                            <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city" value={selectedCity} onChange={handleSelectCity}>
-                                <option value="0">Selecione uma cidade</option>
-                                {cities.map(city => (
-                                    <option key={city} value={city}>{city}</option>
-                                ))}
-                            </select>
-                        </div>
+            <fieldset>
+                <legend>
+                    <h2>Endereço</h2>
+                    <span>Selecione o endereço no mapa</span>
+                </legend>
+                <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    ></TileLayer>
+                    <Marker position={selectedPosition}></Marker>
+                </Map>
+                <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="uf">Estado (UF)</label>
+                        <select name="uf" id="uf" value={selectedUf} onChange={handleSelectUf}>
+                            <option value="0">Selecione uma UF</option>
+                            {ufs.map(uf => (
+                                <option key={uf} value={uf}>{uf}</option>
+                            ))}
+                        </select>
                     </div>
-                </fieldset>
+                    <div className="field">
+                        <label htmlFor="city">Cidade</label>
+                        <select name="city" id="city" value={selectedCity} onChange={handleSelectCity}>
+                            <option value="0">Selecione uma cidade</option>
+                            {cities.map(city => (
+                                <option key={city} value={city}>{city}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </fieldset>
 
-                <fieldset>
-                    <legend>
-                        <h2>Ítens de coleta</h2>
-                    </legend>
-                    <ul className="items-grid">
-                        {items.map(item => (
-                            <li key={item.id}>
-                                <img src={item.image_url} alt="Teste" />
-                                <span>{item.title}</span>
-                            </li>
-                        ))}
-                    </ul>
-                </fieldset>
-                <button type="submit">Cadastrar ponto de coleta</button>
-            </form>
-        </div>
-    );
+            <fieldset>
+                <legend>
+                    <h2>Ítens de coleta</h2>
+                </legend>
+                <ul className="items-grid">
+                    {items.map(item => (
+                        <li key={item.id}>
+                            <img src={item.image_url} alt="Teste" />
+                            <span>{item.title}</span>
+                        </li>
+                    ))}
+                </ul>
+            </fieldset>
+            <button type="submit">Cadastrar ponto de coleta</button>
+        </form>
+    </div>
+);
 };
 
 export default CreatePoint;
